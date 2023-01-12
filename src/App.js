33f55db6932da653cae8357211278ff32a892ccc
raiihan <s.siilavn@yahoo.com>
2023-01-12 21:29:36 +0600

    dependency install

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+
+import { Box } from '@mui/material'
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+
 const App = () => {
     return (
         <>
-            hello
+            <Box width={"400px"}>
+                <Navbar />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/exercise/:id' element={<Home />} />
+                </Routes>
+            </Box>
         </>
     )
 }
+
+export default App
